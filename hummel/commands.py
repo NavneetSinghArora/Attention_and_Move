71cfe764a86dd6b3b8a087eb34f0708d8307a147
dkfusion <12956862+dkfusion@users.noreply.github.com>
2022-05-23 14:03:22 +0200

    changed hummel train

--- hummel/commands.py
+++ hummel/commands.py
@@ -10,4 +10,5 @@ class Hummel:
 
     @staticmethod
     def train(username):
-        subprocess.call(shlex.split('ssh hummel1 "cd \$WORK/logs/ && sbatch \$HOME/jobs/start.sh"'.format(os.path.dirname(os.path.realpath(__file__)),username)))
+        # TODO: bash: sbatch: command not found - because modules will only be loaded in interactive shell! Find solution!
+        subprocess.call(shlex.split('ssh hummel1 "cd /work/{0}/logs/ && sbatch /home/{0}/jobs/start.sh"'.format(username)))
