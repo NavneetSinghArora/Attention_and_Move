72e098491a8b42a0756fd2755209dc8415412055
Aida Usmanova <0usmanov@informatik.uni-hamburg.de>
2022-05-25 14:43:33 +0200

    Revert "Update: clip to return the output (img_features, target_found, similarity)"
    This reverts commit 7d0dd39c2e68128f09b09a9a70ff0ee5cbf3ddcd.

--- src/core/services/clip.py
+++ src/core/services/clip.py
@@ -5,14 +5,14 @@ from PIL import Image
 from torchvision.datasets import CIFAR100
 
 
-def predict(frame, target_object, target_object_threshold, rootDirectory):
+def predict(img, idx, target_object, rootDirectory):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model, preprocess = clip.load('ViT-L/14@336px', device=device, download_root=join(rootDirectory, 'data/external/clip/models/'))
 
     # Download the dataset
     cifar100 = CIFAR100(root=join(rootDirectory, 'data/external/torchvision/datasets/'), download=True, train=False)
 
-    image = preprocess(Image.fromarray(frame)).unsqueeze(0).to(device)
+    image = preprocess(Image.fromarray(img)).unsqueeze(0).to(device)
     text = torch.cat([clip.tokenize(f'a {c}') for c in cifar100.classes]).to(device)
 
     with torch.no_grad():
@@ -21,20 +21,14 @@ def predict(frame, target_object, target_object_threshold, rootDirectory):
 
     # Pick the top 5 most similar labels for the image
     image_features /= image_features.norm(dim=-1, keepdim=True)
-    # print('image features:', image_features.shape)
     text_features /= text_features.norm(dim=-1, keepdim=True)
-    # print('text features:', text_features.shape)
     similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-
     values, indices = similarity[0].topk(10)
 
     # Print the result
-    target_found = False
+    print('Agent ' + str(idx) + ':')
     for value, index in zip(values, indices):
-        # found = ''
-        if cifar100.classes[index] == target_object.lower() and 100 * value.item() >= int(target_object_threshold):
-            # found = ' - found ' + target_object + '!'
-            target_found = True
-        # print(f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%{found}")
-
-    return (image_features, target_found, similarity)
+        found = ''
+        if cifar100.classes[index] == target_object.lower():
+            found = ' - found ' + target_object + '!'
+        print(f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%{found}")
