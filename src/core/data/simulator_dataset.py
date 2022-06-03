ae53ce7d7796724363afc764c2de6052d6a9373c
Aida Usmanova <0usmanov@informatik.uni-hamburg.de>
2022-06-03 19:27:10 +0200

    Update: layers freezed, valiation in progress

--- src/core/data/simulator_dataset.py
+++ src/core/data/simulator_dataset.py
@@ -1,6 +1,5 @@
 from traceback import print_tb
 from torch.utils.data import Dataset
-from clip.simple_tokenizer import SimpleTokenizer
 from PIL import Image
 import numpy as np
 import pandas as pd
@@ -9,49 +8,17 @@ import torch
 
 class SimulatorDataset(Dataset):
 
-    def __init__(self, object_classes, images, annotations, preprocess, dataset_type):
+    def __init__(self,  df, preprocess, dataset_type):
         super(Dataset, self).__init__()
-        self.object_classes = object_classes
-        self.images = images
-        self.annotations = annotations
+        self.df = df
         self.preprocess = preprocess
         self.dataset_type = dataset_type
-        self.tokenizer = SimpleTokenizer()
-        pass
-
-    def tokenize(self, object_classes, context_length: int = 77):
-        sot_token = self.tokenizer.encoder["<|startoftext|>"]
-        eot_token = self.tokenizer.encoder["<|endoftext|>"]
-        all_tokens = [[sot_token] + self.tokenizer.encode(object_class) + [eot_token] for object_class in object_classes]
-        result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
-
-        for i, tokens in enumerate(all_tokens):
-            n = min(len(tokens), context_length)
-            result[i, :n] = torch.tensor(tokens)[:n]
-            if len(tokens) > context_length:
-                result[i, -1] = tokens[-1]
-        return result
-
-    def get_annotation_label(self, annotation_file):
-        texts = []
-        targets = []
-        data = pd.read_csv(annotation_file, names=['label', 'size', 'center', 'cornerPoints'])
-        data_classes = data['label']
-        labels = np.arange(0, len(self.object_classes))
-        for object_class in data_classes:
-            if object_class.lower() in self.object_classes:
-                texts = np.append(texts, object_class.lower())
-                index = self.object_classes.index(object_class.lower())
-                # label[index] = 1
-                targets.append(int(labels[index]))
-        return targets, texts
 
     def __len__(self):
-        return len(self.images)
-        pass
+        return len(self.df)
 
     def __getitem__(self, idx):
-        image = self.preprocess(Image.open('/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/' + self.dataset_type + '/images/' + self.images[idx]))
-        targets, texts = self.get_annotation_label('/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/' + self.dataset_type + '/annotations/' + self.annotations[idx])
-        tokenized_texts = self.tokenize(texts)
-        return image, targets, tokenized_texts
+        image = self.preprocess(Image.open('/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/' + self.dataset_type + '/images/' + self.df['images'][idx]))
+        target = self.df['targets'][idx]
+        text = self.df['texts'][idx]
+        return image, target, text
--- src/core/model/object_detection/clip_detection.py
+++ src/core/model/object_detection/clip_detection.py
@@ -1,10 +1,12 @@
 
+from cgitb import reset
 from PIL import Image
 from pathlib import Path
 from tqdm.auto import tqdm
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import Dataset, DataLoader
 from src.core.data.simulator_dataset import SimulatorDataset
+from clip.simple_tokenizer import SimpleTokenizer
 from os.path import join
 import pandas as pd
 import torch
@@ -30,6 +32,21 @@ class ClipObjectDetection:
         self.learning_rate = 1e-2
         self.momentum = 0.2
         self.criterion = CrossEntropyLoss()
+        self.tokenizer = SimpleTokenizer()
+        self.n = 0
+        self.mean = 0
+        self.batch_size = 32
+
+    def reset_rolling_mean(self):
+        self.n = 0
+        self.mean = 0
+
+    def update_mean(self, value):
+        self.mean = (self.mean * self.n + value) / (self.n + 1)
+        self.n += 1
+
+    def mean_result(self):
+        return self.mean
 
     def get_optimiser(self, optimiser='SGD'):
         if optimiser == 'SGD':
@@ -40,6 +57,44 @@ class ClipObjectDetection:
     def get_scheduler(self, optimiser, number_of_epochs, dataloader):
         return torch.optim.lr_scheduler.OneCycleLR(optimiser, 1e-2, total_steps=number_of_epochs * (2 * len(dataloader) - 1), base_momentum=0.0, max_momentum=0.5, pct_start=0.1, div_factor=1e2, final_div_factor=1e4)
 
+    def tokenize(self, object_class, context_length: int = 77):
+        sot_token = self.tokenizer.encoder["<|startoftext|>"]
+        eot_token = self.tokenizer.encoder["<|endoftext|>"]
+        all_tokens = [[sot_token] + self.tokenizer.encode(object_class) + [eot_token]]
+        result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
+
+        for i, tokens in enumerate(all_tokens):
+            n = min(len(tokens), context_length)
+            result[i, :n] = torch.tensor(tokens)[:n]
+            if len(tokens) > context_length:
+                result[i, -1] = tokens[-1]
+        return result
+
+    def get_annotation_label(self, annotation_file, object_classes):
+        texts = []
+        targets = []
+        data = pd.read_csv(annotation_file, names=['label', 'size', 'center', 'cornerPoints'])
+        data_classes = data['label']
+        labels = np.arange(0, len(object_classes))
+        ind = 0
+        for object_class in data_classes:
+            if object_class.lower() in object_classes:
+                texts.append(object_class.lower())
+                index = object_classes.index(object_class.lower())
+                targets.append(int(labels[index]))
+            ind += 1
+        
+        return targets, texts
+
+    def create_dataframe(self, images, annotations, object_classes, dataset_type):
+        df = pd.DataFrame()
+        for i in range(2):
+            targets, texts = self.get_annotation_label('/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/' + dataset_type + '/annotations/' + annotations[i], object_classes)
+            for j in range(len(targets)):
+                df=df.append({'images':images[i], 'targets':targets[j], 'texts':self.tokenize(texts[j])}, ignore_index=True)
+
+        return df
+
     def get_dataloader(self):
         object_classes = (self.simulator_properties['object_classes']).split(',')
 
@@ -47,103 +102,93 @@ class ClipObjectDetection:
             '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/training/images/')
         training_annotations = os.listdir(
             '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/training/annotations/')
-        training_dataset = SimulatorDataset(object_classes, training_images, training_annotations, self.preprocess, 'training')
-        training_dataloader = DataLoader(training_dataset, batch_size=1, shuffle=True)
+        training_df = self.create_dataframe(training_images, training_annotations, object_classes, 'training')
+        
+        training_dataset = SimulatorDataset(training_df, self.preprocess, 'training')
+        training_dataloader = DataLoader(training_dataset, batch_size=self.batch_size, shuffle=True)
 
         validation_images = os.listdir(
             '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/validation/images/')
         validation_annotations = os.listdir(
             '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/validation/annotations/')
-        validation_dataset = SimulatorDataset(object_classes, validation_images, validation_annotations, self.preprocess, 'validation')
-        validation_dataloader = DataLoader(validation_dataset, batch_size=1, shuffle=True)
+        validation_df = self.create_dataframe(validation_images, validation_annotations, object_classes, 'validation')
+        validation_dataset = SimulatorDataset(validation_df, self.preprocess, 'validation')
+        validation_dataloader = DataLoader(validation_dataset, batch_size=self.batch_size, shuffle=True)
 
-        testing_images = os.listdir(
-            '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/testing/images/')
-        testing_annotations = os.listdir(
-            '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/testing/annotations/')
-        testing_dataset = SimulatorDataset(object_classes, testing_images, testing_annotations, self.preprocess, 'testing')
-        testing_dataloader = DataLoader(testing_dataset, batch_size=1, shuffle=True)
+        # testing_images = os.listdir(
+        #     '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/testing/images/')
+        # testing_annotations = os.listdir(
+        #     '/export2/scratch/cv_proj_team1/Attention_and_Move/output/dataset/testing/annotations/')
+        # testing_dataset = SimulatorDataset(object_classes, testing_images, testing_annotations, self.preprocess, 'testing')
+        # testing_dataloader = DataLoader(testing_dataset, batch_size=self.batch_size, shuffle=True)
 
-        return training_dataloader
+        return training_dataloader, validation_dataloader
 
     def train(self):
-        training_dataloader = self.get_dataloader()
+        training_dataloader, validation_dataloader = self.get_dataloader()
         number_of_epochs = 1
         optimiser = self.get_optimiser()
         scheduler = self.get_scheduler(optimiser, number_of_epochs, training_dataloader)
         for epoch in range(1):
             with tqdm(total=len(training_dataloader) - 1) as bar:
-                # loss_mean = RollingMean()
-                for images, targets, texts in training_dataloader:
-                    for i in range(len(targets)):
-                        images_features = self.model.encode_image(images.to(self.device))
-                        target = targets[i].reshape(1, 1).to(self.device)
-                        texts_features = self.model.encode_text(texts[0][i].reshape(1,77).to(self.device))
+                self.reset_rolling_mean()
+                self.model.train()
+                for batch_images, batch_targets, batch_texts in training_dataloader:
+                    images_features = self.model.encode_image(batch_images.to(self.device))
+                    target = batch_targets.to(self.device)
+                    texts_features = self.model.encode_text(batch_texts.reshape(batch_texts.shape[0], 77).to(self.device))
 
-                        optimiser.zero_grad()
+                    # Freeze all but last layer of first 6 blocks
+                    for name, param in self.model.named_parameters():
+                        split_names = name.split('.')
+                        if len(split_names) < 2:
+                            continue
+                        elif (split_names[1] == 'transformer' and int(split_names[3]) <6) or (split_names[0] == 'transformer' and int(split_names[2]) <6):
+                            if not 'fc' in name:
+                                param.requires_grad = False
+                        
+                    optimiser.zero_grad()
 
-                        # Join train and test features
-                        features = torch.hstack([images_features, texts_features])
+                    # Join train and test features
+                    features = torch.hstack([images_features, texts_features])
 
-                        # L2-normalize features
-                        features = features / features.norm(2, dim=1, keepdim=True)
+                    # L2-normalize features
+                    features = features / features.norm(2, dim=1, keepdim=True)
 
-                        # Apply Cross Entropy Loss SemiHardLoss
-                        loss = self.criterion(features, np.squeeze(target).reshape(1))
-                        loss.backward()
+                    # Apply Cross Entropy Loss SemiHardLoss
+                    # print(torch.nn.Softmax(features).to(self.device))
+                    loss = self.criterion(features, batch_targets)
+                    loss.backward()
                     optimiser.step()
                     scheduler.step()
 
                     # Update metric and progress bar
-                    # loss_mean.update(loss.item())
+                    self.update_mean(loss.item())
                     bar.update()
-                    # bar.set_description('{:.4f}'.format(loss.result()))
+                    bar.set_description('{:.4f}'.format(self.mean_result()))
+            print("Training: ", self.mean_result())
+            
+            with tqdm(total=len(validation_dataloader) - 1) as bar:
+                self.reset_rolling_mean()
+                self.model.eval()
+                for batch_images, batch_targets, batch_texts in validation_dataloader:
 
+                    with torch.no_grad():
+                        images_features = self.model.encode_image(batch_images.to(self.device))
+                        target = batch_targets.to(self.device)
+                        texts_features = self.model.encode_text(batch_texts.reshape(batch_texts.shape[0], 77).to(self.device))
 
-#
-#
-# class RollingMean():
-#     def __init__(self):
-#         self.n = 0
-#         self.mean = 0
-#
-#     def update(self, value):
-#         self.mean = (self.mean * self.n + value) / (self.n + 1)
-#         self.n += 1
-#
-#     def result(self):
-#         return self.mean
-#
-#
-# scheduler = torch.optim.lr_scheduler.OneCycleLR(optim, 1e-2, total_steps=n_epochs * (2*len(dltrain)-1),
-#                                                base_momentum=0.0, max_momentum=0.5, pct_start=0.1, div_factor=1e2, final_div_factor=1e4)
-#
-# for epoch in range(n_epochs):
-#     with tqdm(total=2 * len(dltrain) - 1) as bar:
-#         loss_mean = RollingMean()
-#         for images, texts, targets in dltrain:
-#             targets = targets.to(device)
-#
-#             # Generate train and text features
-#             # images_features = model.encode_image(images.to(device))
-#             # texts_features = model.encode_text(texts.to(device))
-#             #
-#             # optim.zero_grad()
-#             #
-#             # # Join train and test features
-#             # features = torch.hstack([images_features, texts_features])
-#             #
-#             # # L2-normalize features
-#             # features = features / features.norm(2, dim=1, keepdim=True)
-#             #
-#             # # Apply Triplet SemiHardLoss
-#             # loss = criterion(features, targets)
-#             #
-#             # loss.backward()
-#             # optim.step()
-#             # scheduler.step()
-#             #
-#             # # Update metric and progress bar
-#             # loss_mean.update(loss.item())
-#             # bar.update()
-#             # bar.set_description('{:.4f}'.format(loss_mean.result()))
+                    # Join train and test features
+                    features = torch.hstack([images_features, texts_features])
+
+                    # L2-normalize features
+                    features = features / features.norm(2, dim=1, keepdim=True)
+
+                    # Apply Cross Entropy Loss SemiHardLoss
+                    loss = self.criterion(features, batch_targets)
+
+                    # Update metric and progress bar
+                    self.update_mean(loss.item())
+                    bar.update()
+                    bar.set_description('{:.4f}'.format(self.mean_result()))
+            print("Validation: ", self.mean_result())
